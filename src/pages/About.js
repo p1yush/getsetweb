e088d7057551599822e42437cc0f0528fee2c2e5
p1yush <piyush2002op@gmail.com>
2022-08-27 11:45:57 +0530

    changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,5 @@
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
-import Specialization from "../components/Specialization";
 
 const About = () => {
     return (
@@ -10,14 +9,13 @@ const About = () => {
        <div className='about-container'>
        <h1>About Us</h1>
        <p>Get Set Web is a platform for small to medium scale businesses to get themselves digitalized through our website development services. No pre-made templates or designs, Getsetweb will provide you with the customized website as per your choice that best suits your pocket.</p><br/>
-        <p>We are a team of highly dedicated and committed website developers, who will be available to assist their clients, each and every second.</p>
+        <p>We are a team of highly dedicated and committed website developers, who will be available to assist their clients, each and every second.</p><br />
+        <p>We are specialised in designing and developing your own website, customised by you, handled by us. Our website developers will contact you once our business development team connects with you.</p>
      </div>
      <div className='about-image'>
        <img src="images/Specializationimage.png" alt='logo'/>
      </div>
      </div>
-     <hr />
-     <Specialization />
      <Footer />
      </div>
     );
